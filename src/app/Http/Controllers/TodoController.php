89109588525199b07fa15585354988bee2d47fc5
shiro310 <mio.tashiro12@gmail.com>
2023-11-21 16:41:28 +0900

    add: 編集画面ファイルの作成。
    feat: 編集画面表示用と更新処理用のルート定義。詳細画面に編集ボタンの追加。編集する元データ取得の関数と更新処理の関数定義。

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -40,4 +40,19 @@ class TodoController extends Controller
         return view('todo.show', ['todo' => $todo]);
     }
 
+    public function edit($id)
+    {
+        $todo = $this->todo->find($id);
+        return view('todo.edit', ['todo' => $todo]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->all();
+        $todo = $this->todo->find($id);
+        $todo->fill($inputs);
+        $todo->save();
+        return redirect()->route('todo.show', $todo->id);
+    }
+
 }
